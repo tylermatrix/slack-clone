a456a9ab3c75176f5263195479717914be44b700
Tyler Matrix <tylermatrix0@gmail.com>
2021-08-12 15:48:57 -0400

    added and styled sidebar component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div className="app">
       <Router>
         <div>
-          <Switch>
-            <Route path="/" exact>
-              <Header></Header>
-            </Route>
-          </Switch>
+          <Header></Header>
+          <AppBody>
+            <Sidebar></Sidebar>
+            <Switch>
+              <Route path="/" exact>
+                {/* Chat */}
+              </Route>
+            </Switch>
+          </AppBody>
         </div>
       </Router>
     </div>
@@ -19,3 +25,8 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
