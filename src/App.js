88134b9e89a6c7645863fdf315cea6e1ed0fea1e
Tyler Matrix <tylermatrix0@gmail.com>
2021-08-12 15:26:18 -0400

    implemented and styled the header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import Header from "./components/Header";
 function App() {
   return (
     <div className="app">
@@ -9,7 +9,7 @@ function App() {
         <div>
           <Switch>
             <Route path="/" exact>
-              <h1>This is the home page</h1>
+              <Header></Header>
             </Route>
           </Switch>
         </div>
